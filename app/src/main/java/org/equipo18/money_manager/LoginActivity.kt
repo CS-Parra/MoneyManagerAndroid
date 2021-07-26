beb3b894bcd9ad8b6adc51e8ad678c820c4c1ec3
CS-Parra <carlos.parra.s@outlook.com>
2021-07-26 06:20:31 -0500

    Primer commit

---
+++ app/src/main/java/org/equipo18/money_manager/LoginActivity.kt
@@ -0,0 +1,11 @@
+package org.equipo18.money_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login1)
+    }
+}
