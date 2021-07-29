df08b47c232b51685b6c1643bd461216460ff66f
Devmmanuel <plasmmadev@gmail.com>
2021-07-29 17:52:27 -0500

    Se agrego la clase User y Money Manager

---
+++ app/src/main/java/org/equipo18/money_manager/User.kt
@@ -0,0 +1,21 @@
+package org.equipo18.money_manager
+
+class User(
+    private var email: String,
+    private var password: String,
+    private var username: String = "Desconocido") {
+
+    fun getUserName() : String{
+        return username
+    }
+
+    fun getEmail() : String {
+        return email
+    }
+
+    fun getPassword(): String {
+        return password
+    }
+
+}
+
