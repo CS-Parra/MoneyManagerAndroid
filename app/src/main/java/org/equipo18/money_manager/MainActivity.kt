ce2f9cdc14b362832e08c4daeb06495d6f9e0fcf
Devmmanuel <plasmmadev@gmail.com>
2021-07-30 00:59:57 -0500

    Se implemento el Login

--- app/src/main/java/org/equipo18/money_manager/MainActivity.kt
+++ app/src/main/java/org/equipo18/money_manager/MainActivity.kt
@@ -2,9 +2,7 @@ package org.equipo18.money_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
