5aa42fa7707e0ea5b0d74e8a5395faef5a64239f
Devmmanuel <plasmmadev@gmail.com>
2021-08-05 18:32:40 -0500

    Se agregaron usuarios predefinidos y un metodo estatico para validar el E-mail

--- app/src/main/java/org/equipo18/money_manager/MoneyManager.kt
+++ app/src/main/java/org/equipo18/money_manager/MoneyManager.kt
@@ -2,10 +2,19 @@ package org.equipo18.money_manager
 
 class MoneyManager {
 
-    val usersList = mutableListOf<User>()
+    val usersList = mutableListOf(
+        User("devmmanuel@gmail.com","devmmanuel","Emmanuel Eduardo"),
+        User("csparra@gmail.com","csparra","Carlos Parra"),
+        User("gissel@gmail.com","gissel","Gissel Carpinteiro"),
+        User("nat@gmail.com","nat","Natalia"),
+        User("u@gmail.com","u","Usuario"),
+        User("dev@gmail.com","dev","Developer"),
+    )
 
-    fun isEmailValid(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    companion object {
+        fun isEmailValid(email: String): Boolean {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        }
     }
 
 }
